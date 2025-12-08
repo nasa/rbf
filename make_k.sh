21a953144f9a09da0ca5e242c15f2a8fe1b5f8a4
Steven Massey <spham67@yahoo.com>
2025-12-08 16:05:11 -0500

    Add files via upload
    update k modules

--- make_k.sh
+++ make_k.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 module purge
-module load intel_2019.2.187 
-module load tecplot360ex-2018R2_64 
+module load intel/2019.5.281
+module load tecplot/2025R1
 module list >& modules.log
 exe_type=ifort
 rm -f make.inc
