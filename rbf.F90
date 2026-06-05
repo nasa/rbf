3ac37a469b118f1a957eebaa540d41d796d7e7ee
Steven Massey <spham67@yahoo.com>
2026-06-05 15:18:21 -0400

    Detect solver failure. Add 3rd solver type dgelsd.

--- rbf.F90
+++ rbf.F90
@@ -5,7 +5,7 @@ module rbf
   private 
 
   public :: get_rbf_weights, get_rbf_interp
-  public :: build_rbf_matrix, solve_rbf_lu
+  public :: build_rbf_matrix, solve_rbf_lu, solve_rbf_lsd
 
 contains
 
@@ -15,7 +15,7 @@ contains
     use phi,      only : phi_tps,  phi_imq
     use svd,      only : solve_svd
 
-    integer ::  info
+    integer ::  info, rank
     integer,  intent(in)  :: nspace
     integer,  intent(in)  :: ndata
     real(dp), intent(in)  :: xdata(nspace,ndata)
@@ -31,7 +31,6 @@ contains
         r(j) = sqrt(sum((xdata(:,i)-xdata(:,j))**2))
       end do
       call phi_tps(ndata, r, r0, v)
-      !call phi_imq(ndata, r, r0, v)
       a(i,:) = v(:)
     end do
 
@@ -42,13 +41,27 @@ contains
       end do
     end do
 
-#ifdef USESVD
-    call solve_svd(ndata, ndata, a, fdata, w) ! more robust, but much slower
+#if defined(USESVD)
+    call solve_svd(ndata, ndata, a, fdata, w)        ! robust, slow
+#elif defined(USELSD)
+    call solve_rbf_lsd(ndata, a, fdata, w, rank, info)
+    if (info /= 0) then
+      print *, 'ERROR: dgelsd failed, info =', info, ' falling back to SVD'
+      call solve_svd(ndata, ndata, a, fdata, w)
+    else if (rank < ndata) then
+      print *, 'WARNING: rank-deficient system, rank=', rank, ' / ', ndata
+    end if
 #else
     call solve_rbf_lu(ndata, a, fdata, w, info)
     if (info /= 0) then
-        print *, 'ERROR: dgesv failed, info =', info, 'calling svd now'
-        call solve_svd(ndata, ndata, a, fdata, w) ! more robust, but much slower
+      print *, 'WARNING: dgesv failed, info =', info, ' trying dgelsd'
+      call solve_rbf_lsd(ndata, a, fdata, w, rank, info)
+      if (info /= 0) then
+        print *, 'ERROR: dgelsd also failed, info =', info, ' calling SVD'
+        call solve_svd(ndata, ndata, a, fdata, w)
+      else if (rank < ndata) then
+        print *, 'WARNING: rank-deficient system, rank=', rank, ' / ', ndata
+      end if
     end if
 #endif
 
@@ -60,7 +73,7 @@ contains
     use phi,      only : phi_tps, phi_imq
     use kinddefs, only : dp
 
-    integer,  intent(in)  :: nspace 
+    integer,  intent(in)  :: nspace
     integer,  intent(in)  :: ndata
     real(dp), intent(in)  :: xdata(nspace,ndata)
     integer,  intent(in)  :: ninterp
@@ -103,24 +116,111 @@ contains
   end subroutine build_rbf_matrix
 
   subroutine solve_rbf_lu(ndata, a, b, x, info)
-    use kinddefs, only: dp, i4
+    use kinddefs, only: dp
     implicit none
-    integer, intent(in) :: ndata
-    real(dp), intent(in) :: a(ndata, ndata)
-    real(dp), intent(in) :: b(ndata)
-    real(dp), intent(out):: x(ndata)
+    integer,  intent(in)  :: ndata
+    real(dp), intent(in)  :: a(ndata, ndata)
+    real(dp), intent(in)  :: b(ndata)
+    real(dp), intent(out) :: x(ndata)
+    integer,  intent(out) :: info
 
-    integer :: ipiv(ndata), info
+    integer  :: ipiv(ndata)
     real(dp) :: aa(ndata, ndata), bb(ndata)
-    ! Make copies because LAPACK overwrites input arrays
+    real(dp) :: resid, bnorm, xnorm, tol
+
     aa = a
     bb = b
     call dgesv(ndata, 1, aa, ndata, ipiv, bb, ndata, info)
     x = bb
-    !if (info /= 0) then
-    !    print *, 'ERROR: dgesv failed, info =', info
-    !end if
+
+    if (info /= 0) return     ! caller will handle fallback
+
+    ! ---- Independent residual check -------------------------------------
+    ! Compute ||A*x - b|| / ||b||  using the ORIGINAL a, b (not overwritten)
+    bnorm = sqrt(sum(b*b))
+    xnorm = sqrt(sum(x*x))
+    resid = sqrt(sum((matmul(a, x) - b)**2))
+    if (bnorm > 0.0_dp) resid = resid / bnorm
+
+    ! Tolerance: scale by problem size and condition tolerance.
+    ! 1e-6 is generous; tighten to 1e-8 or 1e-10 to be stricter.
+    tol = 1.0e-6_dp
+
+    print '(a,es12.4,a,es12.4,a,es12.4)',                                       &
+          ' dgesv check: ||Ax-b||/||b|| =', resid,                              &
+          '  ||x|| =', xnorm, '  ||b|| =', bnorm
+
+    if (resid > tol .or. xnorm > 1.0e12_dp * max(bnorm,1.0_dp)) then
+       print *, 'WARNING: dgesv returned info=0 but residual is bad'
+       print *, '         flagging as failure (info=-999)'
+       info = -999    ! signal caller to use fallback
+    end if
   end subroutine solve_rbf_lu
 
+  ! ---------------------------------------------------------------------
+  ! New: solve A x = b using LAPACK dgelsd (SVD-based least squares).
+  ! Handles rank-deficient / ill-conditioned systems gracefully by
+  ! truncating singular values smaller than rcond * sigma_max.
+  ! ---------------------------------------------------------------------
+  subroutine solve_rbf_lsd(ndata, a, b, x, rank, info)
+    use kinddefs, only: dp
+    implicit none
+    integer,  intent(in)  :: ndata
+    real(dp), intent(in)  :: a(ndata, ndata)
+    real(dp), intent(in)  :: b(ndata)
+    real(dp), intent(out) :: x(ndata)
+    integer,  intent(out) :: rank
+    integer,  intent(out) :: info
+
+    real(dp), allocatable :: aa(:,:), bb(:), s(:), work(:)
+    integer,  allocatable :: iwork(:)
+    real(dp) :: rcond, wopt(1)
+    integer  :: m, n, nrhs, lda, ldb, lwork, liwork, iwopt(1)
+
+    m    = ndata
+    n    = ndata
+    nrhs = 1
+    lda  = ndata
+    ldb  = ndata
+
+    ! Cutoff: singular values < rcond * sigma_max are treated as zero.
+    ! 1e-12 is a reasonable default for double precision RBF problems;
+    ! tighten or loosen as needed.
+    rcond = 1.0e-12_dp
+
+    allocate(aa(lda,n), bb(max(m,n)), s(min(m,n)))
+    aa = a
+    bb = 0.0_dp
+    bb(1:m) = b
+
+    ! ---- Workspace query ----
+    lwork  = -1
+    liwork = -1
+    call dgelsd(m, n, nrhs, aa, lda, bb, ldb, s, rcond, rank,                 &
+                wopt, lwork, iwopt, info)
+    if (info /= 0) then
+      print *, 'dgelsd workspace query failed, info=', info
+      deallocate(aa, bb, s)
+      return
+    end if
+    lwork  = int(wopt(1))
+    liwork = iwopt(1)
+    allocate(work(lwork), iwork(liwork))
+
+    ! ---- Actual solve ----
+    call dgelsd(m, n, nrhs, aa, lda, bb, ldb, s, rcond, rank,                 &
+                work, lwork, iwork, info)
+
+    if (info == 0) then
+      x = bb(1:n)
+      print '(a,i0,a,i0,a,es12.4,a,es12.4)',                                  &
+            ' dgelsd: rank=', rank, '/', n,                                   &
+            '  s_max=', s(1), '  s_min=', s(min(m,n))
+    else
+      print *, 'dgelsd solve failed, info=', info
+    end if
+
+    deallocate(aa, bb, s, work, iwork)
+  end subroutine solve_rbf_lsd
 
 end module rbf
